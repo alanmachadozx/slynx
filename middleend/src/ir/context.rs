b763fc6f50a1a0c65f1e30b3a32d265068c742b6
cykna <kina.lima1302@proton.me>
2026-03-09 22:30:42 -0300

    refactor: mini refactor on code organization

--- middleend/src/ir/context.rs
+++
@@ -1,9 +0,0 @@
-use crate::{IRPointer, IRTypeId, ir::label::Label};
-
-///A context is anything that can be executed. It contains labels and each determine what to do, the '$entry' label is the label that is initally executed when this context initializes to be executed, for sure, this after compilation
-pub struct Context {
-    ///Named labels that can have instructions and determine what on the code to be executed. The first label this points to is the `$entry` label
-    labels: IRPointer<Label>,
-    ///The type of the context. Must be either Function
-    ty: IRTypeId,
-}
